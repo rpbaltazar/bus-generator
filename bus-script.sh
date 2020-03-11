8bed06ca7362902404bb654f485db800d5e91024
Rui Baltazar <rui.p.baltazar@gmail.com>
2020-03-11 16:07:01 +0800

    [] Grab text from argv

--- bus-script.sh
+++ bus-script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 base="thrown-under-bus.jpg"
 result="out.jpg"
-text="Nicolas"
+text=$1
 x=395
 y=435
 #
 convert "$base" -fill white -size 320x100 -pointsize 20 -stroke white -annotate 35x35+${x}+${y} "$text" "$result"
+open out.jpg
